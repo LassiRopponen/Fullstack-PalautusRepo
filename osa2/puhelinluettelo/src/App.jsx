f3c0323ee6c17e5eb2982388874aa0ba5a407685
Lassi Ropponen <lassi.ropponen@tuni.fi>
2024-07-03 12:00:14 +0300

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, handler}) => (
   <form>
@@ -10,7 +11,7 @@ const Person = ({person}) => (
   <p>{person.name} {person.number}</p>
 )
 
-const Persons = ({persons, filter}) => (
+const Persons = ({persons}) => (
   persons.map(person => <Person key={person.name} person={person} />)
 )
 
@@ -23,15 +24,18 @@ const PersonForm = ({add, newName, newNumber, nameHandler, numberHandler}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => (setNewName(event.target.value))
   const handleNumberChange = (event) => (setNewNumber(event.target.value))
@@ -70,7 +74,7 @@ const App = () => {
         numberHandler={handleNumberChange} 
       />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} filter={filter} />
+      <Persons persons={personsToShow} />
     </div>
   )
 
