351fa59266c61597c2db68b27aff53c4be4d5165
Lassi Ropponen <lassi.ropponen@tuni.fi>
2024-07-30 16:57:11 +0300

    4.18-4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,17 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
-  
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-  
-    const users = await User.find({})
-    const user = users[0]
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
